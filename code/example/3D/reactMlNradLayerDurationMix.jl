d48775c3e823e9bd76aed17b8dfd42847a3721f4
Monkie Dein Hau Jia Lin <hjl_monkey_1997@hotmail.com>
2023-06-12 21:44:18 -0400

    update mix
    update mix

--- code/example/3D/reactMlNradLayerDurationMix.jl
+++ code/example/3D/reactMlNradLayerDurationMix.jl
@@ -5,11 +5,10 @@ using StatsBase
 wpInitsArray = [0.8,0.95] 
 layerRadisArray = [0,51,57.19,62.11,66.21,69.76,72.89,75.72,78.31,80.68,82.88] #round.(Int,)
 Tgvalues = [25.4,45.1,102.1]
-histCounts = []
+colorpalletes = palette(:cool, length(Tgvalues))
+TgIndexes = [1;1:3;1:3;1:3]
 N = 1000                                                                                   # N : NumberOfRadical
-Vs = fill(0.0,(length(Tgvalues),length(wpInitsArray),N))
-histograms = []
-for (ntg,tgv) in enumerate(Tgvalues)
+# for (ntg,tgv) in enumerate(Tgvalues)
 # ntg = 3
 # tgv = Tgvalues[ntg]
 for (nwp,wpInit) in enumerate(wpInitsArray)
@@ -21,7 +20,8 @@ startWpTime , endWpTime = 0.0 , 30*60.0                        # linear Relation
 parRadius = layerRadisArray[length(layerRadisArray)]                           # particle radius
 reactionTemp = 70                                               # T : Reaction Temparature (°C)
 layerRadius = layerRadisArray[1:(length(layerRadisArray)-1)]    # 2 # (1+nwp)
-Tg₀ = fill(tgv,size(layerRadius))                              # initial Tg
+Tg₀ = Tgvalues[TgIndexes] # fill(tgv,size(layerRadius))                              # initial Tg
+colors = colorpalletes[TgIndexes]
 Tmon = 106                                                      # monomer temp 106°C
 
 # --- Make particle observable function --- 
@@ -62,22 +62,36 @@ counts = countmap(reduce(vcat,simsLayer))
 values = sort(collect(keys(counts)))
 frequencies = [counts[value] for value in values]
 duration = frequencies ./ sum(frequencies) .* T
-barplot = bar(values, duration, xlabel="Layer", ylabel="Duration (second)",ylim=(0,T), label = "", 
+barplot = bar(values, duration, xlabel="Layer", ylabel="Duration (second)",ylim=(0,T), label = "Histogram", 
 title="Histogram of radicles time spend in each Layer",xticks=1:(length(layerRadisArray)-1),xlim=(0.5,length(layerRadisArray)-0.5))
-savefig(barplot,wdir("plots/MlayerChgWp/Cliff/TimeSpend/")*string(wpInit)*"-wp-Layer-tg-"*string(tgv)*".png")
+for l in 1:length(layerRadius)
+    BoxShape(barplot,[l-0.5,l+0.5],[0,T];bw= 1,lc=plot_color(colors[l], 0.1),c = plot_color(colors[l], 0.1))
+end
+for tgval in 1:length(Tgvalues)
+    BoxShape(barplot,[-4,-5],[-4,-5];bw= 1,lc=plot_color(colorpalletes[tgval], 0.1),c = plot_color(colorpalletes[tgval], 0.1),label="Tg: "*string(Tgvalues[tgval])*"°C")
+end
+savefig(barplot,wdir("plots/MlayerChgWp/Cliff/TimeSpend/")*string(wpInit)*"-wp-Layer-tg-mix.png")
+# savefig(barplot,wdir("plots/MlayerChgWp/Cliff/TimeSpend/")*string(wpInit)*"-wp-Layer-tg-"*string(tgv)*".png")
 
-########################################################################
 v = floor.(Int,reduce(vcat,simsL2))
 num_bins = max(1,Int(floor(maximum(v) - minimum(v))))
 counts = countmap(v)
+
 # Extract unique values and their frequencies
 values = sort(collect(keys(counts)))
 frequencies = [counts[value] for value in values]
 duration = frequencies ./ sum(frequencies) .* T
-his = bar(values, duration, xlabel="Distance (nm)", ylabel="Duration (second)",ylim=(0,T), label = "", 
+his = bar(values, duration, xlabel="Distance (nm)", ylabel="Duration (second)",ylim=(0,T), label = "Histogram", 
 title="Histogram of radicles time spend in each Distance",xticks=0:5:layerRadisArray[length(layerRadisArray)],xlim=(0,layerRadisArray[length(layerRadisArray)]))
-savefig(his,wdir("plots/MlayerChgWp/Cliff/TimeSpend/")*string(wpInit)*"-wp-Dist-tg-"*string(tgv)*".png")
+for l in 1:length(layerRadius)
+    BoxShape(his,layerRadisArray[[l,l+1]],[0,T];bw= 1,lc=plot_color(colors[l], 0.1),c = plot_color(colors[l], 0.1))
+end
+for tgval in 1:length(Tgvalues)
+    BoxShape(his,[-4,-5],[-4,-5];bw= 1,lc=plot_color(colorpalletes[tgval], 0.1),c = plot_color(colorpalletes[tgval], 0.1),label="Tg: "*string(Tgvalues[tgval])*"°C")
+end
+savefig(his,wdir("plots/MlayerChgWp/Cliff/TimeSpend/")*string(wpInit)*"-wp-Dist-tg-mix.png")
+
 
 end
-end
+# end
 
