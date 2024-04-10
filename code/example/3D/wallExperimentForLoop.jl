2c2e4e34b1e8275c7cce9f7c067cb54cca98e9da
Monkie Dein Hau Jia Lin <hjl_monkey_1997@hotmail.com>
2024-04-10 10:42:28 -0400

    move location

--- code/example/3D/wallExperimentForLoop.jl
+++ code/example/3D/wallExperimentForLoop.jl
@@ -78,7 +78,7 @@ for glossy in 0:totalLayer
     his = Plots.histogram(v, normalize=true,bins=num_bins,xticks=0:5:layerRadisArray[length(layerRadisArray)],
     ytick=0:0.1:1.0,ylim=(0,1.0),xlim=(0,layerRadisArray[length(layerRadisArray)]))
 
-    savefig(his,wdir("plots/MlayerChgWp/$wpInit/total$totalLayer/glossy$glossy")*"/DeepestHistogram.png")
+    savefig(his,wdir("plots/MlayerChgWp/wallExp/$wpInit/total$totalLayer/glossy$glossy")*"/DeepestHistogram.png")
 end
 end
 
